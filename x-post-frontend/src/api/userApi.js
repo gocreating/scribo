30725368668835127622cb1723c286f6062ae1f7
翁治平 <gocreating@gmail.com>
2018-10-01 01:46:06 +0800

    Add user signin api

--- x-post-frontend/src/api/userApi.js
+++ x-post-frontend/src/api/userApi.js
@@ -4,6 +4,9 @@ class UserApi extends ApiClient {
   signup(user) {
     return this.post('/app-users', { data: user })
   }
+  signin(user) {
+    return this.post('/app-users/login', { data: user })
+  }
 }
 
 let userApi = new UserApi()
