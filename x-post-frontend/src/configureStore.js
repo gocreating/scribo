fc091928bbc31be32afd0295c686930e04fee448
翁治平 <gocreating@gmail.com>
2018-09-30 16:21:39 +0800

    Add redux-persist to maintain auth states

--- x-post-frontend/src/configureStore.js
+++ x-post-frontend/src/configureStore.js
@@ -9,15 +9,23 @@ import {
   routerMiddleware,
 } from 'connected-react-router'
 import { createBrowserHistory } from 'history'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 import { reducer as formReducer } from 'redux-form'
 import thunk from 'redux-thunk'
 
 let history = createBrowserHistory()
+let persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['auth'],
+}
 let rootReducer = combineReducers({
   form: formReducer,
 })
+let persistedReducer = persistReducer(persistConfig, rootReducer)
 let store = createStore(
-  connectRouter(history)(rootReducer),
+  connectRouter(history)(persistedReducer),
   {},
   compose(
     applyMiddleware(
@@ -26,10 +34,12 @@ let store = createStore(
     )
   )
 )
+let persistor = persistStore(store)
 
 export default () => {
   return {
     store: store,
     history: history,
+    persistor: persistor,
   }
 }
--- x-post-frontend/src/index.js
+++ x-post-frontend/src/index.js
@@ -1,15 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
+import { PersistGate } from 'redux-persist/integration/react'
 import registerServiceWorker from './registerServiceWorker'
 import configureStore from './configureStore'
 import App from './App'
 
-let { store, history } = configureStore()
+let { store, history, persistor } = configureStore()
 
 ReactDOM.render((
   <Provider store={store}>
-    <App history={history} />
+    <PersistGate loading={null} persistor={persistor}>
+      <App history={history} />
+    </PersistGate>
   </Provider>
 ), document.getElementById('root'))
 registerServiceWorker()
