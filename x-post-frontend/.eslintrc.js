abb018ef855f55070ca19f22257bef36cc5f7329
翁治平 <gocreating@gmail.com>
2018-09-30 22:55:59 +0800

    Add linting

---
+++ x-post-frontend/.eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  "parser": "babel-eslint",
+  "parserOptions": {
+    "ecmaVersion": 8,
+    "sourceType": "module",
+    "ecmaFeatures": {
+      "experimentalObjectRestSpread": true,
+      "jsx": true,
+    }
+  },
+  "plugins": [
+    "react",
+    "react-native",
+    "class-property",
+  ],
+  "extends": [
+    "eslint:recommended",
+    "plugin:react/recommended",
+  ],
+  "rules": {
+    "object-curly-spacing": ["error", "always"],
+    "semi": ["error", "never"],
+  },
+  "globals": {
+    "alert": true,
+    "FormData": true,
+  }
+}
