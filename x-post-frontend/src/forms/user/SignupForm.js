9fd92887dc5d6bd5a9b9cd0b81fd8817eedc08fb
翁治平 <gocreating@gmail.com>
2018-09-30 18:24:56 +0800

    Refactor SignupForm as function component

--- x-post-frontend/src/forms/user/SignupForm.js
+++ x-post-frontend/src/forms/user/SignupForm.js
@@ -1,36 +1,30 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { Field, reduxForm } from 'redux-form'
 import { Button, Form } from 'semantic-ui-react'
 import Input from '../../fields/Input'
 import FormTypes from '../../constants/FormTypes'
 
-class SignupForm extends Component {
-  render() {
-    let { onSubmit, handleSubmit } = this.props
-
-    return (
-      <Form onSubmit={handleSubmit(onSubmit)}>
-        <Form.Field>
-          <label>Email</label>
-          <Field
-            name="email"
-            component={Input}
-            type="text"
-          />
-        </Form.Field>
-        <Form.Field>
-          <label>Password</label>
-          <Field
-            name="password"
-            component={Input}
-            type="password"
-          />
-        </Form.Field>
-        <Button>Signup</Button>
-      </Form>
-    )
-  }
-}
+let SignupForm= ({ onSubmit, handleSubmit }) => (
+  <Form onSubmit={handleSubmit(onSubmit)}>
+    <Form.Field>
+      <label>Email</label>
+      <Field
+        name="email"
+        component={Input}
+        type="text"
+      />
+    </Form.Field>
+    <Form.Field>
+      <label>Password</label>
+      <Field
+        name="password"
+        component={Input}
+        type="password"
+      />
+    </Form.Field>
+    <Button>Signup</Button>
+  </Form>
+)
 
 export default reduxForm({
   form: FormTypes.USER_SIGNUP,
