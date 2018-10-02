3f61359ec02147e4ae2e8b22ccf2db353521576c
翁治平 <gocreating@gmail.com>
2018-10-03 01:55:14 +0800

    Add list user post API

--- x-post-frontend/src/api/postApi.js
+++ x-post-frontend/src/api/postApi.js
@@ -1,6 +1,9 @@
 import ApiClient from './ApiClient'
 
 class PostApi extends ApiClient {
+  list(userId) {
+    return this.get(`/app-users/${userId}/posts`)
+  }
   create(userId, post) {
     return this.post(`/app-users/${userId}/posts`, { data: post })
   }
--- x-post-frontend/src/ducks/post.js
+++ x-post-frontend/src/ducks/post.js
@@ -3,10 +3,22 @@ import postApi from '../api/postApi'
 
 // Action Creators
 const plainActionCreators = createActions({
+  POST_LIST_API_SUCCESS: (res) => ({ res }),
+  POST_LIST_API_FAILURE: (res) => ({ res }),
   POST_CREATE_API_SUCCESS: (res) => ({ res }),
   POST_CREATE_API_FAILURE: (res) => ({ res }),
 })
 const thunkActionCreators = {
+  postListApiRequest: (userId) => async (dispatch) => {
+    try {
+      let response = await postApi.list(userId)
+      dispatch(postListApiSuccess(response))
+      return response.body
+    } catch ({ response }) {
+      dispatch(postListApiFailure(response))
+      return response.body
+    }
+  },
   postCreateApiRequest: (userId, post) => async (dispatch) => {
     try {
       let response = await postApi.create(userId, post)
@@ -22,7 +34,10 @@ const thunkActionCreators = {
 export const {
   postCreateApiSuccess,
   postCreateApiFailure,
+  postListApiSuccess,
+  postListApiFailure,
 } = plainActionCreators
 export const {
   postCreateApiRequest,
+  postListApiRequest,
 } = thunkActionCreators
