8f46248f607b40bd6cb9ac884890171fcda5607b
翁治平 <gocreating@gmail.com>
2018-10-04 22:42:07 +0800

    Update postList API

--- x-post-frontend/src/api/postApi.js
+++ x-post-frontend/src/api/postApi.js
@@ -1,8 +1,8 @@
 import ApiClient from './ApiClient'
 
 class PostApi extends ApiClient {
-  list(userId) {
-    return this.get(`/app-users/${userId}/posts`)
+  list(userId, ...rest) {
+    return this.get(`/app-users/${userId}/posts`, ...rest)
   }
   create(userId, post) {
     return this.post(`/app-users/${userId}/posts`, { data: post })
