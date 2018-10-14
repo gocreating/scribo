170a82258b493752566c1d66f27feaa7471f2f12
翁治平 <gocreating@gmail.com>
2018-10-15 01:49:53 +0800

    Render markdown's code as pure component

--- x-post-frontend/src/editor/blocks/Markdown/renderers/Code.js
+++ x-post-frontend/src/editor/blocks/Markdown/renderers/Code.js
@@ -1,22 +1,20 @@
-import React, { Component } from 'react'
+import React from 'react'
 import CodeHighlight from '../../CodeHighlight/CodeHighlight'
 import Themes from '../../CodeHighlight/Themes'
 
-class Code extends Component {
-  render() {
-    let { language, value } = this.props
-    let block = {
-      values: {
-        code: value,
-        theme: Themes.SOLARIZED_LIGHT,
-        language: language.toUpperCase(),
-      },
-    }
-
-    return (
-      <CodeHighlight block={block} />
-    )
+let Code = (props) => {
+  let { language, value } = props
+  let block = {
+    values: {
+      code: value,
+      theme: Themes.SOLARIZED_LIGHT,
+      language: language.toUpperCase(),
+    },
   }
+
+  return (
+    <CodeHighlight block={block} />
+  )
 }
 
 export default Code
