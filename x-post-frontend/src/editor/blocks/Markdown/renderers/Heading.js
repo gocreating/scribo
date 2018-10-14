4a4feb9b9dc9f1e2a67aeab2b3a1d8c0cd154d0d
翁治平 <gocreating@gmail.com>
2018-10-15 01:52:52 +0800

    Render markdown's heading as pure component

--- x-post-frontend/src/editor/blocks/Markdown/renderers/Heading.js
+++ x-post-frontend/src/editor/blocks/Markdown/renderers/Heading.js
@@ -1,21 +1,19 @@
-import React, { Component } from 'react'
+import React from 'react'
 import Header from '../../Header/Header'
 
-class Heading extends Component {
-  render() {
-    let { level, children } = this.props
-    let block = {
-      values: {
-        level,
-      },
-    }
-
-    return (
-      <Header block={block}>
-        {children}
-      </Header>
-    )
+let Heading = (props) => {
+  let { level, children } = props
+  let block = {
+    values: {
+      level,
+    },
   }
+
+  return (
+    <Header block={block}>
+      {children}
+    </Header>
+  )
 }
 
 export default Heading
