e6813f72f02cead1c12e03d2de8eba288aced770
翁治平 <gocreating@gmail.com>
2018-10-27 21:18:12 +0800

    Improve paragraph typesetting

--- x-post-frontend/src/editor/blocks/Paragraph/Paragraph.js
+++ x-post-frontend/src/editor/blocks/Paragraph/Paragraph.js
@@ -1,22 +1,89 @@
-import React from 'react'
-import './Paragraph.css'
+import React, { Component } from 'react'
 import pangu from 'pangu'
+import Han from 'han-css'
+import Typesettings from './Typesettings'
+import 'han-css/dist/han.min.css'
+import './Paragraph.css'
 
-let Paragraph = ({ block, children }) => (
-  children ? (
-    <span className="paragraph content web-font">
-      {children}
-    </span>
-  ) : (
-  block.values.text
-    .split('\n')
-    .map((line, idx) => (
-      <span className="paragraph content web-font" key={`${idx}-${line}`}>
-        {pangu.spacing(line)}
-        <br/>
-      </span>)
-    )
-  )
-)
+class Paragraph extends Component {
+  paragraphRef = React.createRef()
+
+  componentDidMount() {
+    let { block, children } = this.props
+    let { typesetting } = block.values
+
+    if (typesetting === Typesettings.HAN || children) {
+      Han(this.paragraphRef.current).render()
+    }
+  }
+
+  render() {
+    let { block, children } = this.props
+    let { typesetting } = block.values
+
+    switch(typesetting) {
+      case Typesettings.RAW: {
+        return (
+          block.values.text
+            .split('\n')
+            .map((line, idx) => {
+              return (
+                <span className="paragraph content web-font" key={`${idx}-${line}`}>
+                  {line}
+                  <br/>
+                </span>
+              )
+            })
+        )
+      }
+
+      case Typesettings.HAN: {
+        return (
+          <div ref={this.paragraphRef}>
+            {block.values.text
+              .split('\n')
+              .map((line, idx) => {
+                return (
+                  <span className="paragraph content web-font" key={`${idx}-${line}`}>
+                    {line}
+                    <br/>
+                  </span>
+                )
+              })}
+          </div>
+        )
+      }
+
+      case Typesettings.PANGU: {
+        return (
+          block.values.text
+            .split('\n')
+            .map((line, idx) => {
+              return (
+                <span className="paragraph content web-font" key={`${idx}-${line}`}>
+                  {pangu.spacing(line)}
+                  <br/>
+                </span>
+              )
+            })
+        )
+      }
+
+      default: {
+        return (
+          <span className="paragraph content web-font" ref={this.paragraphRef}>
+            {children}
+          </span>
+        )
+      }
+    }
+  }
+}
+
+Paragraph.defaultProps = {
+  block: {
+    values: {},
+  }
+}
 
 export default Paragraph
