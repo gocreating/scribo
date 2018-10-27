c30f084c8d43dd29a875decffedcd99439b062ae
翁治平 <gocreating@gmail.com>
2018-10-27 19:05:37 +0800

    Replace font icon with svg icon

--- x-post-frontend/src/layouts/Footer.js
+++ x-post-frontend/src/layouts/Footer.js
@@ -1,5 +1,20 @@
 import React from 'react'
-import { Container, Grid, Segment, List, Header, Icon } from 'semantic-ui-react'
+import { Container, Grid, Segment, List, Header } from 'semantic-ui-react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faFacebook,
+  faInstagram,
+  faLinkedin,
+  faYoutube,
+  faPinterest,
+  faHtml5,
+  faCss3,
+  faJs,
+  faNodeJs,
+  faReact,
+  faSass,
+  faGithub,
+} from '@fortawesome/free-brands-svg-icons'
 import './Footer.scss'
 
 let Footer = () => (
@@ -15,24 +30,24 @@ let Footer = () => (
         <Grid.Column>
           <Segment basic compact>
             <List horizontal size="big">
-              <List.Item><Icon name="facebook" /></List.Item>
-              <List.Item><Icon name="instagram" /></List.Item>
-              <List.Item><Icon name="linkedin" /></List.Item>
-              <List.Item><Icon name="youtube" /></List.Item>
-              <List.Item><Icon name="pinterest square" /></List.Item>
+              <List.Item><FontAwesomeIcon icon={faFacebook} /></List.Item>
+              <List.Item><FontAwesomeIcon icon={faInstagram} /></List.Item>
+              <List.Item><FontAwesomeIcon icon={faLinkedin} /></List.Item>
+              <List.Item><FontAwesomeIcon icon={faYoutube} /></List.Item>
+              <List.Item><FontAwesomeIcon icon={faPinterest} /></List.Item>
             </List>
           </Segment>
         </Grid.Column>
         <Grid.Column>
           <Segment basic compact>
             <List horizontal size="big">
-              <List.Item><Icon name="html5" /></List.Item>
-              <List.Item><Icon name="css3" /></List.Item>
-              <List.Item><Icon name="js" /></List.Item>
-              <List.Item><Icon name="node js" /></List.Item>
-              <List.Item><Icon name="react" /></List.Item>
-              <List.Item><Icon name="sass" /></List.Item>
-              <List.Item><Icon name="github" /></List.Item>
+              <List.Item><FontAwesomeIcon icon={faHtml5} /></List.Item>
+              <List.Item><FontAwesomeIcon icon={faCss3} /></List.Item>
+              <List.Item><FontAwesomeIcon icon={faJs} /></List.Item>
+              <List.Item><FontAwesomeIcon icon={faNodeJs} /></List.Item>
+              <List.Item><FontAwesomeIcon icon={faReact} /></List.Item>
+              <List.Item><FontAwesomeIcon icon={faSass} /></List.Item>
+              <List.Item><FontAwesomeIcon icon={faGithub} /></List.Item>
             </List>
           </Segment>
         </Grid.Column>
