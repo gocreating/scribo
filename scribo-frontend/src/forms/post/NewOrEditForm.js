8a8c26f63b41ab500ed877f0000a1059d469ea97
翁治平 <gocreating@gmail.com>
2019-03-05 17:17:48 +0800

    Add `seriesPosts` to submission attributes

--- scribo-frontend/src/forms/post/NewOrEditForm.js
+++ scribo-frontend/src/forms/post/NewOrEditForm.js
@@ -134,6 +134,7 @@ class NewOrEditForm extends Component {
   };
 
   handleSubmit = (targetConsumerFn, targetSubmitButton) => (data) => {
+    let { seriesPosts, ...formData } = data
     let blocks = this.xeditor.current
       .getBlocks()
       .map(block => ({
@@ -141,12 +142,14 @@ class NewOrEditForm extends Component {
         type: block.type,
         values: block.values,
       }))
+    let seriesPostIds = (seriesPosts || []).map(seriesPost => seriesPost.id)
 
     this.setState({ targetSubmitButton })
     // return a promise to trigger redux-form's `submitting` prop
     return new Promise((resolve, reject) => {
       resolve(targetConsumerFn({
-        ...data,
+        ...formData,
+        seriesPosts: seriesPostIds,
         blocks,
       }))
     })
